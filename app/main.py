ca2df38d840ea1e248089ef94c6d8dea4d584c41
MohitAryal <cozahitman@gmail.com>
2025-07-22 20:04:53 +0545

    Added main

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from models import Base
+from database import engine
+from routes import router as api_router
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+app.include_router(api_router)
